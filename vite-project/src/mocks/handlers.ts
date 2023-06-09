0eec672a6bab32c7efad07e811c9b23c19790d7f
Lotje <lotte-moors@hotmail.com>
2023-06-09 14:45:22 +0200

    feat: handlers

--- vite-project/src/mocks/handlers.ts
+++ vite-project/src/mocks/handlers.ts
@@ -1,2 +1,21 @@
-/* istanbul ignore file */
-export const handlers = [];
+import { rest } from "msw";
+
+export const handlers = [
+  // Handles a POST request
+  rest.post("https://fancy-app.com/postToThisEndpoint", (req, res, ctx) => {
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200)
+    );
+  }),
+
+  // Handles a GET request
+  rest.get("https://fancy-app.com/getSomeData", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: "fancy data string",
+      })
+    );
+  }),
+];
